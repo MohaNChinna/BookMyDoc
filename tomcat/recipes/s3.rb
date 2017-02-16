6e6626a1d7c3dc8bb44f8b548a5017cbf2c12258
MohaNChinna <mohan.solaris0606@gmail.com>
2017-02-16 18:44:01 +0530

    Update s3.rb

--- tomcat/recipes/s3.rb
+++ tomcat/recipes/s3.rb
@@ -1,10 +1,8 @@
-s3_file "/tmp/weather_reporting.war" do
-  bucket "icp-cf-test"
-  remote_path "weather_reporting.war"
+s3_file "/var/lib/tomcat6/webapps/BookMyDocv4.war" do
+  bucket "opsworkmohan"
+  remote_path "BookMyDocv4.war"
   mode '0644'
   action :create
   aws_access_key_id node[:custom_access_key]
   aws_secret_access_key node[:custom_secret_key]
-  decryption_key "my SHA256 digest key"
-  decrypted_file_checksum "SHA256 hex digest of decrypted file"
 end
