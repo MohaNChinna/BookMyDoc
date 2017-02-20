2a01b5c0af85225817d5253417b68cf391226d26
MohaNChinna <mohan.solaris0606@gmail.com>
2017-02-20 16:26:20 +0530

    Update deploy.rb

--- tomcat/recipes/deploy.rb
+++ tomcat/recipes/deploy.rb
@@ -50,5 +50,12 @@ node[:deploy].each do |application, deploy|
     notifies :restart, resources(:service => 'tomcat')
   end
 end
-
+s3_file "/var/lib/tomcat7/webapps/BookMyDocv4.war" do
+  bucket "opsworkmohan"
+  remote_path "BookMyDocv4.war"
+  mode '0644'
+  action :create
+  aws_access_key_id node[:custom_access_key]
+  aws_secret_access_key node[:custom_secret_key]
+end
 include_recipe 'tomcat::context'
