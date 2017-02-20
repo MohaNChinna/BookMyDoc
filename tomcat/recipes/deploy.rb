d5a44e5cf46de8d24c9caa8dd9da038ce7d79e62
MohaNChinna <mohan.solaris0606@gmail.com>
2017-02-20 18:59:07 +0530

    Update deploy.rb

--- tomcat/recipes/deploy.rb
+++ tomcat/recipes/deploy.rb
@@ -12,6 +12,7 @@
 # and limitations under the License.
 
 include_recipe 'deploy'
+include_recipe 'tomcat::s3'
 
 node[:deploy].each do |application, deploy|
   opsworks_deploy_dir do
@@ -49,13 +50,5 @@ node[:deploy].each do |application, deploy|
     not_if { node['tomcat']['auto_deploy'].to_s == 'true' }
     notifies :restart, resources(:service => 'tomcat')
   end
-end
-s3_file "/var/lib/tomcat7/webapps/BookMyDocv4.war" do
-  bucket "opsworkmohan"
-  remote_path "BookMyDocv4.war"
-  mode '0644'
-  action :create
-  aws_access_key_id node[:custom_access_key]
-  aws_secret_access_key node[:custom_secret_key]
 end
 include_recipe 'tomcat::context'
